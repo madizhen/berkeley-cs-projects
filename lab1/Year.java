f29a451b3c63e1fc1c4b3da8c0e1588b89da596c
Madison Draper <mzhen@berkeley.edu>
2017-11-01 15:14:42 -0700

    1959

---
+++ lab1/Year.java
@@ -0,0 +1,31 @@
+public class Year {
+	static boolean isLeapYear(int year) {
+		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//Calls isLeapYear to print if leap year or not
+	private static void checkLeapYear(int year) {
+		if (isLeapYear(year)) {
+			System.out.printf("%d is a leap year.\n", year);
+		} else {
+			System.out.printf("%d is not a leap year.\n", year);
+		}
+	}
+	//Comand line integer argument
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Please enter command line arguments. \n e.g. java Year 2017");
+		}
+		for (int i = 0; i < args.length; i++) {
+			try {
+				int year = Integer.parseInt(args[i]);
+				checkLeapYear(year);
+			} catch (NumberFormatException e) {
+				System.out.printf("%s is not a valid number.\n", args[i]);
+			}
+		}
+	}
+}
